82c243719aa8ea71dc5f858daa15fd4c1b743159
Andy Gray <andy@whatsthescore.co.uk>
2018-10-09 14:55:20 +0100

    marketing preference component

---
+++ src/components/MarketingPreference.vue
@@ -0,0 +1,66 @@
+<template>
+    <b-card title="Receive News" sub-title="Keep up-to-date with all the latest Investx news" class="shadow-sm">
+        <p class="card-text" v-if="userData && userData.marketing">
+            You are currently opted in to receive news.
+            <br/>
+            <b-button-group class="mx-1 mt-3">
+                <b-button size="sm" variant="outline-secondary" v-on:click="optOut">Opt out</b-button>
+            </b-button-group>
+        </p>
+
+        <p class="card-text" v-else-if="userData && !userData.marketing">
+            You are currently <strong>not</strong> opted in to receive news.
+            <br/>
+            <b-button-group class="mx-1 mt-3">
+                <b-button size="sm" variant="primary" v-on:click="optIn">Send me the news</b-button>
+            </b-button-group>
+        </p>
+
+        <b-alert show variant="warning" v-else>
+            Please select your news preference
+            <br/>
+            <b-button-group class="mx-1 mt-3">
+                <b-button size="sm" variant="primary" v-on:click="optIn">Send me the news</b-button>
+            </b-button-group>
+            <b-button-group class="mx-1 mt-3">
+                <b-button size="sm" variant="outline-secondary">No thanks</b-button>
+            </b-button-group>
+        </b-alert>
+    </b-card>
+</template>
+
+
+<script>
+    import firebase from 'firebase';
+    import { db } from '../main';
+    import { mapState } from 'vuex';
+
+    export default {
+        name: 'marketing-preference',
+        computed: {
+            ...mapState([
+                'userData'
+            ])
+        },
+        methods: {
+            forgot: function (evt) {
+                evt.preventDefault();
+
+                firebase.auth().sendPasswordResetEmail(this.email)
+                    .then((/*user*/) => this.message = 'Reset password email sent')
+                    .catch((err) => console.error('Oops. ' + err.message));
+            },
+            optIn: function () {
+                db.ref(`users/${firebase.auth().currentUser.uid}/marketing`).set(true);
+            },
+            optOut: function () {
+                db.ref(`users/${firebase.auth().currentUser.uid}/marketing`).set(false);
+            },
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    @import '../assets/scss/inx-colours.scss';
+
+</style>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -13,34 +13,7 @@
                     <personal-information></personal-information>
                 </div>
                 <div class="col">
-                    <b-card title="Receive News" sub-title="Keep up-to-date with all the latest Investx news" class="shadow-sm">
-                        <p class="card-text" v-if="userData && userData.marketing">
-                            You are currently opted in to receive news.
-                            <br/>
-                            <b-button-group class="mx-1 mt-3">
-                                <b-button size="sm" variant="outline-secondary" v-on:click="optOut">Opt out</b-button>
-                            </b-button-group>
-                        </p>
-
-                        <p class="card-text" v-else-if="userData && !userData.marketing">
-                            You are currently <strong>not</strong> opted in to receive news.
-                            <br/>
-                            <b-button-group class="mx-1 mt-3">
-                                <b-button size="sm" variant="primary" v-on:click="optIn">Send me the news</b-button>
-                            </b-button-group>
-                        </p>
-
-                        <b-alert show variant="warning" v-else>
-                            Please select your news preference
-                            <br/>
-                            <b-button-group class="mx-1 mt-3">
-                                <b-button size="sm" variant="primary" v-on:click="optIn">Send me the news</b-button>
-                            </b-button-group>
-                            <b-button-group class="mx-1 mt-3">
-                                <b-button size="sm" variant="outline-secondary">No thanks</b-button>
-                            </b-button-group>
-                        </b-alert>
-                    </b-card>
+                    <marketing-preference></marketing-preference>
                 </div>
             </div>
 
@@ -76,13 +49,14 @@
     // @ is an alias to /src
     import Sidebar from '@/components/Sidebar';
     import PersonalInformation from '@/components/PersonalInformation';
+    import MarketingPreference from '@/components/MarketingPreference';
     import firebase from 'firebase';
     import { db } from '../main';
     import { mapState } from 'vuex';
 
     export default {
         name: 'settings',
-        components: {Sidebar, PersonalInformation},
+        components: {Sidebar, PersonalInformation, MarketingPreference},
         data () {
             const user = firebase.auth().currentUser;
             return {
